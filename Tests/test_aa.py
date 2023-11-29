01dbda70b75cb2a3bcfa9de493e935c8d551ce95
Martin Genet <martin.genet@polytechnique.edu>
2023-11-29 15:42:40 +0100

    test

---
+++ Tests/test_aa.py
@@ -0,0 +1,7 @@
+print("import gmsh", flush=True)
+import gmsh
+print("import dolfin_mech", flush=True)
+import dolfin_mech
+print("gmsh.initialize()", flush=True)
+gmsh.initialize()
+print("done", flush=True)
---
+++ Tests/test_ab.py
@@ -0,0 +1,7 @@
+print("import dolfin_mech", flush=True)
+import dolfin_mech
+print("import gmsh", flush=True)
+import gmsh
+print("gmsh.initialize()", flush=True)
+gmsh.initialize()
+print("done", flush=True)
