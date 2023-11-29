1322b865fc9977b394ab064e36ef758f93ed217f
mmanooch <mahdi.manoochehrtayebi@inria.fr>
2023-11-29 18:26:53 +0100

    Revert "Multi-step loading"
    This reverts commit ef5b2f82992c5c676d12aface934f8ffb29b0075.

--- dolfin_mech/__init__.py
+++ dolfin_mech/__init__.py
@@ -82,7 +82,6 @@ from .run_HeartSlice_Mesh import *
 from .run_HollowBox_Homogenization import *
 from .run_HollowBox_Mesh import *
 from .run_HollowBox_MicroPoroHyperelasticity import *
-from .run_HollowBox_MicroPoroHyperelasticity_steps import *
 from .run_RivlinCube_Elasticity import *
 from .run_RivlinCube_Hyperelasticity import *
 from .run_RivlinCube_Mesh import *
