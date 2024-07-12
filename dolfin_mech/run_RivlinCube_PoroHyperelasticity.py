e7da1377b7dc175d1d1b92f7ecd572aa7c7e7c7f
Alice Peyraut <alice.peyraut@polytechnique.edu>
2024-07-12 19:20:16 +0200

    fixing tests syntax

--- dolfin_mech/run_RivlinCube_PoroHyperelasticity.py
+++ dolfin_mech/run_RivlinCube_PoroHyperelasticity.py
@@ -22,10 +22,10 @@ def run_RivlinCube_PoroHyperelasticity(
         inverse=0,
         cube_params={},
         porosity_params={},
+        move_params={},
         mat_params={},
         step_params={},
         load_params={},
-        move={},
         inertia=0,
         res_basename="run_RivlinCube_PoroHyperelasticity",
         plot_curves=False,
@@ -48,8 +48,8 @@ def run_RivlinCube_PoroHyperelasticity(
         elif (dim==3):
             mesh, boundaries_mf, xmin_id, xmax_id, ymin_id, ymax_id, zmin_id, zmax_id = dmech.run_RivlinCube_Mesh(dim=dim, params=cube_params)
 
-    if move.get("move", False) == True :
-        Umove = move.get("U")
+    if move_params.get("move", False) == True :
+        Umove = move_params.get("U")
         dolfin.ALE.move(mesh, Umove)
 
     ################################################################ Porosity ###
