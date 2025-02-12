388824e683fee870921467706d73604c3243d0bc
mmanooch <mahdi.manoochehrtayebi@inria.fr>
2025-02-12 14:24:55 +0100

    Sigma_ZZ=0 fixed!

--- dolfin_mech/Material_Elastic_ExponentialNeoHookean.py
+++ dolfin_mech/Material_Elastic_ExponentialNeoHookean.py
@@ -39,8 +39,6 @@ class ExponentialNeoHookeanElasticMaterial(ElasticMaterial):
                 self.Sigma =  self.beta1 * (self.kinematics.I - self.kinematics.C_inv) * (self.kinematics.IC - 2 - 2*dolfin.ln(self.kinematics.J))**(self.alpha - 1) * dolfin.exp(self.beta2*(self.kinematics.IC - 2 - 2*dolfin.ln(self.kinematics.J))**self.alpha) \
                            +  2 * self.beta3 * (self.kinematics.I - self.kinematics.C_inv)\
                            +  2*self.beta4 * (self.kinematics.J**2 - 1) * self.kinematics.C_inv 
-                self.Sigma_ZZ = dolfin.Constant(0.)
-                self.p_hydro = -(dolfin.tr(self.Sigma.T*self.kinematics.C)+ self.Sigma_ZZ)/3/self.kinematics.J
         
         elif (self.kinematics.dim == 3):
             self.Psi   =  self.beta1/self.beta2/self.alpha/2 * (dolfin.exp(self.beta2*(self.kinematics.IC - 3 - 2*dolfin.ln(self.kinematics.J))**self.alpha) - 1) \
@@ -49,10 +47,7 @@ class ExponentialNeoHookeanElasticMaterial(ElasticMaterial):
             self.Sigma =  self.beta1 * (self.kinematics.I - self.kinematics.C_inv) * (self.kinematics.IC - 3 - 2*dolfin.ln(self.kinematics.J))**(self.alpha - 1) * dolfin.exp(self.beta2*(self.kinematics.IC - 3 - 2*dolfin.ln(self.kinematics.J))**self.alpha) \
                         +  2 * self.beta3 * (self.kinematics.I - self.kinematics.C_inv)\
                         +  2*self.beta4 * (self.kinematics.J**2 - 1) * self.kinematics.C_inv 
-            self.p_hydro = -(dolfin.tr(self.Sigma.T*self.kinematics.C))/3/self.kinematics.J
 
 
         self.P = self.kinematics.F * self.Sigma
         self.sigma = self.P * self.kinematics.F.T / self.kinematics.J
-        self.Sigma_dev = self.Sigma + self.p_hydro * self.kinematics.J * self.kinematics.C_inv
-        self.Sigma_VM = dolfin.sqrt(1.5 *dolfin.tr(self.Sigma_dev.T*self.Sigma_dev))
