f43bc83704211bd223976a285c0e2d324f21363c
Haotian <haotian.xiao@outlook.com>
2026-03-23 14:38:13 +0100

    for merge request

--- Tests/test_microporohyperelasticity.py
+++ Tests/test_microporohyperelasticity.py
@@ -15,28 +15,19 @@
 
 #################################################################### imports ###
 
-import dolfin
-import math
-import numpy
 import sys
-import os
-from pathlib import Path
-local_path = Path(__file__).resolve().parent.parent
-sys.path.insert(0, str(local_path))
-from fenics import *
-import dolfin_mech as dmech
-from dolfin_mech.Problem_Hyperelasticity_MicroPoroFlow import MicroPoroFlowHyperelasticityProblem
 
 import myPythonLibrary as mypy
+import dolfin_mech     as dmech
 
 ####################################################################### test ###
 
 res_folder = sys.argv[0][:-3]
 test = mypy.Test(
     res_folder=res_folder,
-    perform_tests=0,
+    perform_tests=1,
     stop_at_failure=1,
-    clean_after_tests=0,
+    clean_after_tests=1,
     tester_numpy_tolerance=1e-2)
 
 dim_lst  = [ ]
@@ -71,8 +62,8 @@ for dim in dim_lst:
                  for j in range (dim):
                     load_params["sigma_bar_"+str(i)+str(j)] = 0.
             elif (load == "macroscopic_stretch"):
-                load_params["pf"] = 0.2
-                load_params["U_bar_00"] = 0.0
+                load_params["pf"] = 0.
+                load_params["U_bar_00"] = 0.5
                 for i in range(dim):
                  for j in range (dim):
                   if ((i != 0) or (j != 0)):
@@ -86,12 +77,12 @@ for dim in dim_lst:
 
             dmech.run_HollowBox_MicroPoroHyperelasticity(
                 dim=dim,
-                mesh_params={"dim":dim, "xmin":0., "ymin":0., "zmin":0., "xmax":1., "ymax":1., "zmax":1., "xshift":-0.3, "yshift":-0.3, "zshift":-0.3, "r0":0.2, "l":0.02, "mesh_filebasename":res_folder+"/"+"mesh"},
+                mesh_params={"dim":dim, "xmin":0., "ymin":0., "zmin":0., "xmax":1., "ymax":1., "zmax":1., "xshift":-0.3, "yshift":-0.3, "zshift":-0.3, "r0":0.2, "l":0.1, "mesh_filebasename":res_folder+"/"+"mesh"},
                 mat_params={"model":"CGNHMR", "parameters":{"E":1.0, "nu":0.3}},
                 bcs=bcs,
                 step_params={"dt_ini":1e-1, "dt_min":1e-3},
                 load_params=load_params,
-                res_basename=res_basename,
-                verbose=1)
+                res_basename=res_folder+"/"+res_basename,
+                verbose=0)
 
             test.test(res_basename)
